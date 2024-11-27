a0ca8f150f40964ece9ee2a4e32321abf968f4a5
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-11-27 12:25:36 +0200

    remove unused code

--- BuyTime_Application/Dto/StudentDto.cs
+++ BuyTime_Application/Dto/StudentDto.cs
@@ -2,7 +2,6 @@ namespace BuyTime_Application.Dto;
 
 public class StudentDto
 {
-    // public string Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }
