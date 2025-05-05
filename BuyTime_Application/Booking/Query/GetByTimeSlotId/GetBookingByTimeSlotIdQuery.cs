94e77395f3c922c7b5f74e29eb17a3d7b51e0c35
Jeweler228 <sweetkerry26@gmail.com>
2025-05-06 02:35:39 +0300

    added booking/getByTimeSlotId

---
+++ BuyTime_Application/Booking/Query/GetByTimeSlotId/GetBookingByTimeSlotIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using BuyTime_Application.Dto;
+using ErrorOr;
+using MediatR;
+
+namespace BuyTime_Application.Booking.Query.GetByTimeSlotId;
+
+public record GetBookingsByTimeSlotIdQuery(Guid TimeSlotId) : IRequest<ErrorOr<List<BookingDto>>>;
---
+++ BuyTime_Application/Booking/Query/GetByTimeSlotId/GetBookingByTimeSlotIdQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using BuyTime_Application.Common.Interfaces.IUnitOfWork;
+using BuyTime_Application.Dto;
+using ErrorOr;
+using Mapster;
+using MediatR;
+
+namespace BuyTime_Application.Booking.Query.GetByTimeSlotId;
+
+public class GetBookingsByTimeSlotIdQueryHandler(IUnitOfWork unitOfWork)
+    : IRequestHandler<GetBookingsByTimeSlotIdQuery, ErrorOr<List<BookingDto>>>
+{
+    public async Task<ErrorOr<List<BookingDto>>> Handle(GetBookingsByTimeSlotIdQuery request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var bookings = await unitOfWork.Booking.GetBookingsByTimeSlotIdAsync(request.TimeSlotId);
+            if (bookings.IsError)
+            {
+                return bookings.Errors;
+            }
+            var bookingDtos = bookings.Value.Adapt<List<BookingDto>>();
+            return bookingDtos;
+        }
+        catch (Exception ex)
+        {
+            return Error.Failure($"Error while retrieving bookings: {ex.Message}");
+        }
+    }
+}
